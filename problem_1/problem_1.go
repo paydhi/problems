da0d9e22ddc8cf53a4de8d2350f6c4b2f48e68f6
Paydhi <paydhi@gmail.com>
2022-06-12 02:33:17 +0300

    Problem 1 solved in Golang

---
+++ problem_1/problem_1.go
@@ -0,0 +1,16 @@
+// Created by: Paydhi
+package main
+
+import "fmt"
+
+func main() {
+	x := 0
+	i := 1
+	for i < 1000 {
+		i = i + 1
+		if (i%3 == 0 || i%5 == 0) {
+			x = x + i
+		}
+	}
+	fmt.Println(x)
+}
